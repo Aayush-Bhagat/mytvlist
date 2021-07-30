8712c05163bc9b4f88bc4525fae4f4a2894474c1
Aayush Bhagat <lamblesus@gmail.com>
2021-07-29 22:34:39 -0400

    small change to userPaths

--- Server/controllers/showPaths.js
+++ Server/controllers/showPaths.js
@@ -3,7 +3,6 @@ import tvShowModel from '../models/tvShow.js'
 export const getAllShows = async (req, res) => {
     try {
         const allShows = await tvShowModel.find()
-        res.header("Access-Control-Allow-Origin", "*");
         res.status(200).json(allShows)
     } catch (error) {
         res.status(404).json({message: error.message})
